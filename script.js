819cd25f5ee071adb236baba12b9794d2de3b0d8
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2025-05-23 18:55:54 -0400

    refactor to classes

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let mylibrary = []
-
-function Book(title, author, pages, read, id) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = id
+//  function Book(title, author, pages, read, id) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.id = id
+    
+// }
+class Book {
+    constructor(title, author, pages, read, id) {
+        this.title = title;
+        this.author = author; 
+        this.pages = pages;
+        this.read = read;
+        this.id = id;
+    }
     
 }
 
